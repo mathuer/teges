7d82f20efd343b342e8394c740aa08644a7f898a
Malin <mal93in@gmail.com>
2018-02-19 09:48:13 +0100

    update

--- theme.js
+++ theme.js
@@ -17,8 +17,34 @@ Bliss.Theme.HandleHeaderScroll = function() {
 	});
 };
 
+
+Bliss.Theme.HandleColumnTransform = function(){
+	if( !$("#content-wrapper .page").length ){
+		return;
+	}
+	
+	$(".column-transform").each(function(){
+		var column = $(this);
+		var class_list = $(this).attr("class").split(" ");
+
+		column_class = class_list[class_list.length-1];
+		
+		var split = column_class.split("-");
+		var col_size = split[split.length-1];
+		
+					
+		column.removeClass( column_class );
+		column.addClass( "col-md-" + col_size);
+
+
+	});
+}
+
+
+
 // Create an instace of your function.
 $(function($) {
 	Bliss.Theme.HandleHeaderScroll();
+	Bliss.Theme.HandleColumnTransform();
 } );
 
